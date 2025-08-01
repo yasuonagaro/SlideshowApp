3a4c72b4b7c87a9d0af7f5e5e3fcc28b0381a7d8
yasuonagaro <yasuo.nagaro@gmail.com>
2025-08-02 02:56:51 +0900

    課題提出

---
+++ SlideshowApp/ZoomViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ZoomViewController.swift
+//  SlideshowApp
+//
+//  Created by ynagaro on 2025/08/02.
+//
+
+import UIKit
+
+class ZoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ZoomInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ZoomInViewController.swift
+//  SlideshowApp
+//
+//  Created by ynagaro on 2025/08/02.
+//
+
+import UIKit
+
+class ZoomInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
