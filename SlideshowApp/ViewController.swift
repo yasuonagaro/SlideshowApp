bd0db44f790b713ad525bad56929f03bc5f0d0c3
yasuonagaro <yasuo.nagaro@gmail.com>
2025-08-01 23:38:33 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by ynagaro on 2025/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SlideshowAppTests/SlideshowAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlideshowAppTests.swift
+//  SlideshowAppTests
+//
+//  Created by ynagaro on 2025/08/01.
+//
+
+import Testing
+@testable import SlideshowApp
+
+struct SlideshowAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
